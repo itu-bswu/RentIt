b873c3ac4808044a444e7823c82e3e52823999a7
Niklas Hansen <nikl@itu.dk>
2012-03-30 00:22:30 +0200

    Cleaned up some documentation.

--- RentItService/Services/ContentService.cs
+++ RentItService/Services/ContentService.cs
@@ -7,9 +7,8 @@
 namespace RentItService.Services
 {
     using System;
-
-    using RentItService.Entities;
-    using RentItService.Interfaces;
+    using Entities;
+    using Interfaces;
 
     /// <summary>
     /// Service for the content providers.
@@ -17,29 +16,33 @@ namespace RentItService.Services
     public partial class Service : IContentService
     {
         /// <summary>
-        /// Operation used to update movie information.
+        /// Update information for a movie. 
+        /// The movie is identified by the ID in the instance of the Movie class. 
+        /// The other information is updated with the new values.
         /// </summary>
-        /// <param name="token">The user token.</param>
+        /// <param name="token">The session token.</param>
         /// <param name="movieObject">The Movie object containing the ID of the movie to be changed and the updated information.</param>
         /// <exception cref="NotImplementedException">Not Yet Implemented.</exception>
         /// <author></author>
         public void EditMovieInformation(string token, Movie movieObject)
         {
             // TODO: Implement EditMovieInformation
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         /// <summary>
-        /// Deletes a movie from the service.
+        /// Deletes a movie from the service. 
+        /// The movie is identified by the ID in the instance of the Movie class. 
+        /// The other properties in the Movie instance are ignored.
         /// </summary>
-        /// <param name="token">The user token.</param>
+        /// <param name="token">The session token.</param>
         /// <param name="movieObject">The movie to be deleted.</param>
         /// <exception cref="NotImplementedException">Not Yet Implemented.</exception>
         /// <author></author>
         public void DeleteMovie(string token, Movie movieObject)
         {
             // TODO: Implement DeleteMovie
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
     }
 }
--- RentItService/Services/GetMovieDataService.cs
+++ RentItService/Services/GetMovieDataService.cs
@@ -1,17 +1,24 @@
-﻿
+﻿//-------------------------------------------------------------------------------------------------
+// <copyright file="GetMovieDataService.cs" company="RentIt">
+// Copyright (c) RentIt. All rights reserved.
+// </copyright>
+//-------------------------------------------------------------------------------------------------
+
 namespace RentItService.Services
 {
     using System;
     using System.Collections.Generic;
-
-    using RentItService.Entities;
-    using RentItService.Interfaces;
+    using Entities;
+    using Interfaces;
 
     /// <summary>
+    /// The movie information service.
     /// </summary>
     public partial class Service : IGetMovieData
     {
-        /// <summary>Gets information about a specific movie.</summary>
+        /// <summary>
+        /// Gets information about a specific movie.
+        /// </summary>
         /// <param name="token">The session token.</param>
         /// <param name="movieId">The ID of the movie to get.</param>
         /// <returns>A movie object equivalent to the entry in the database.</returns>
@@ -22,27 +29,33 @@ namespace RentItService.Services
             throw new NotImplementedException();
         }
 
-        /// <summary>Gets the most downloaded movies.</summary>
+        /// <summary>
+        /// Gets the most downloaded movies.
+        /// </summary>
         /// <param name="token">The session token.</param>
         /// <returns>An IEnumerable containing the most downloaded movies.</returns>
         /// <exception cref="NotImplementedException">Not Yet Implemented.</exception>
         public IEnumerable<Movie> GetMostDownloaded(string token)
         {
             // TODO: Implement GetMostDownloaded
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
-        /// <summary>Gets the genres in the database.</summary>
+        /// <summary>
+        /// Gets all the genres currently applied to the movies in the database.
+        /// </summary>
         /// <param name="token">The session token.</param>
         /// <returns>An IEnumerable containing all the genres in the database.</returns>
         /// <exception cref="NotImplementedException">Not Yet Implemented.</exception>
         public IEnumerable<string> GetAllGenres(string token)
         {
             // TODO: Implement GetAllGenres
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
-        /// <summary>Filters the list of movies into a particular genre.</summary>
+        /// <summary>
+        /// Filters the list of movies into a particular genre.
+        /// </summary>
         /// <param name="token">The session token.</param>
         /// <param name="genre">The genre to filter by.</param>
         /// <returns>An IEnumerable containing the filtered movies.</returns>
@@ -50,10 +63,12 @@ namespace RentItService.Services
         public IEnumerable<Movie> GetMoviesByGenre(string token, string genre)
         {
             // TODO: Implement GetMoviesByGenre
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
-        /// <summary>Searches the database for a specific movie title.</summary>
+        /// <summary>
+        /// Searches the database for a specific movie title.
+        /// </summary>
         /// <param name="token">The session token.</param>
         /// <param name="search">The search string.</param>
         /// <returns>An IEnumerable containing the movies fitting the search.</returns>
@@ -61,7 +76,7 @@ namespace RentItService.Services
         public IEnumerable<Movie> Search(string token, string search)
         {
             // TODO: Implement Search
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
     }
 }
