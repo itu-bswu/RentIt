0809bd289370f864411e79f4439e007e3a5792ef
Jacob Grooss <jcgr@itu.dk>
2012-04-07 19:06:11 +0200

    Added GetUsers and GetContentPublishers along with respective tests. Also split own tests into seperate files for better overview

---
+++ Test/Scenarios/GetMovieDataService/GetMovieInformationTest.cs
@@ -0,0 +1,91 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetMovieInformationTest.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the GetMovieInformationTest type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace RentIt.Tests.Scenarios.GetMovieData
+{
+    using System.Linq;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using RentItService;
+    using RentItService.Entities;
+    using RentItService.Exceptions;
+    using RentItService.Services;
+
+    /// <summary>
+    /// Class for testing the GetMovieInformation method
+    /// </summary>
+    [TestClass]
+    public class GetMovieInformationTest : DataTest
+    {
+        /// <summary>Tests the GetMovieInformation with valid inputs</summary>
+        [TestMethod]
+        public void GetMovieInformationTest1()
+        {
+            Service service = new Service();
+
+            using (var db = new RentItContext())
+            {
+                TestHelper.SetUpTestMovies();
+                TestHelper.SetUpTestUsers();
+
+                User testUser = db.Users.First(u => u.Username == "testUser");
+                Movie testMovie = db.Movies.First(u => u.Title == "testMovie1");
+
+                Movie foundMovie = service.GetMovieInformation(testUser.Token, testMovie.ID);
+
+                Assert.AreEqual(testMovie.ID, foundMovie.ID);
+                Assert.AreEqual(testMovie.Title, foundMovie.Title);
+                Assert.AreEqual(testMovie.Description, foundMovie.Description);
+                Assert.AreEqual(testMovie.Genre, foundMovie.Genre);
+            }
+        }
+
+        /// <summary>
+        /// Tests if an exception is thrown when an invalid token is given
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(UserNotFoundException))]
+        public void InvalidTokenGetMovieInformationTest()
+        {
+            Service service = new Service();
+
+            using (var db = new RentItContext())
+            {
+                TestHelper.SetUpTestMovies();
+                TestHelper.SetUpTestUsers();
+
+                Movie testMovie = db.Movies.First(u => u.Title == "testMovie1");
+
+                Movie foundMovie = service.GetMovieInformation("Hello thar", testMovie.ID);
+            }
+        }
+
+        /// <summary>
+        /// Tests if null is returned with an invalid movieId or not
+        /// </summary>
+        [TestMethod]
+        public void InvalidMovieIdGetMovieInformation()
+        {
+            Service service = new Service();
+
+            using (var db = new RentItContext())
+            {
+                TestHelper.SetUpTestMovies();
+                TestHelper.SetUpTestUsers();
+
+                User testUser = db.Users.First(u => u.Username == "testUser");
+
+                Movie foundMovie = service.GetMovieInformation(testUser.Token, 1337);
+
+                Assert.IsNull(foundMovie);
+            }
+        }
+    }
+}
---
+++ Test/Scenarios/UserInformationService/GetContentPublishersTest.cs
@@ -0,0 +1,95 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetContentProvidersTest.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the GetContentProvidersTest type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace RentIt.Tests.Scenarios.UserInformationService
+{
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using RentItService;
+    using RentItService.Entities;
+    using RentItService.Enums;
+    using RentItService.Exceptions;
+    using RentItService.Services;
+
+    /// <summary>
+    /// Class for testing the GetContentProviders method
+    /// </summary>
+    [TestClass]
+    public class GetContentPublishersTest : DataTest
+    {
+        /// <summary>
+        /// Tests the GetContentPublishers method
+        /// </summary>
+        [TestMethod]
+        public void GetContentProvidersTest1()
+        {
+            Service service = new Service();
+
+            using (var db = new RentItContext())
+            {
+                TestHelper.SetUpTestUsers();
+
+                User testAdmin = db.Users.First(u => u.Username == "testAdmin");
+                User testProvider = db.Users.First(u => u.Username == "testContentProvider");
+
+                Assert.IsNotNull(testProvider);
+
+                var amountOfPublishers = Enumerable.Count(db.Users, user => user.Type == UserType.ContentProvider);
+                var publisherList = service.GetContentPublishers(testAdmin.Token);
+
+                Assert.IsNotNull(publisherList);
+                Assert.AreEqual(amountOfPublishers, publisherList.Count());
+
+                publisherList = service.GetContentPublishers(testAdmin.Token);
+
+                Assert.IsNotNull(publisherList);
+                Assert.AreEqual(testProvider.ID, publisherList.First(u => u.Username == "testContentProvider").ID);
+            }
+        }
+
+        /// <summary>
+        /// Tests if the method throws the right exception when
+        /// calling it with a wrong user type
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InsufficientAccessLevelException))]
+        public void InvalidUserTypeTest()
+        {
+            Service service = new Service();
+
+            using (var db = new RentItContext())
+            {
+                TestHelper.SetUpTestUsers();
+
+                User testUser = db.Users.First(u => u.Username == "testUser");
+
+                var userList = service.GetContentPublishers(testUser.Token);
+            }
+        }
+
+        /// <summary>
+        /// Tests if the method throws the right exception when
+        /// calling it with an invalid token
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(UserNotFoundException))]
+        public void InvalidTokenTest()
+        {
+            Service service = new Service();
+
+            using (var db = new RentItContext())
+            {
+                TestHelper.SetUpTestUsers();
+
+                var userList = service.GetContentPublishers("bneiuwnvu9p28h3ny84o28uyh43892");
+            }
+        }
+    }
+}
