04fa762f0df1eb163ff81c74d0cf36c3f422c2b8
Ulrik Damm <ulrikdamm@me.com>
2012-04-07 16:39:49 +0200

    added two scenario tests

---
+++ Test/Scenarios/SearchScenarioTest.cs
@@ -0,0 +1,53 @@
+﻿namespace RentIt.Tests.Scenarios
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using RentItService;
+    using RentItService.Entities;
+    using RentItService.Exceptions;
+    using RentItService.Services;
+
+    [TestClass]
+    public class SearchScenarioTest : DataTest
+    {
+        [TestMethod]
+        public void SearchExcactTitle()
+        {
+            TestHelper.SetUpTestUsers();
+            TestHelper.SetUpTestMovies();
+
+            var service = new Service();
+
+            using (var db = new RentItContext())
+            {
+                User user = db.Users.First(u => u.Username == "testUser");
+
+                var movies = service.Search(user.Token, "testMovie1");
+
+                Assert.IsTrue(movies.Single().Title.Equals("testMovie1"));
+            }
+        }
+
+        [TestMethod]
+        public void SearchPartlyTitle()
+        {
+            TestHelper.SetUpTestUsers();
+            TestHelper.SetUpTestMovies();
+
+            var service = new Service();
+
+            using (var db = new RentItContext())
+            {
+                User user = db.Users.First(u => u.Username == "testUser");
+
+                var movies = service.Search(user.Token, "Movie");
+
+                Assert.IsTrue(movies.Single().Title.Equals("testMovie1"));
+            }
+        }
+    }
+}
