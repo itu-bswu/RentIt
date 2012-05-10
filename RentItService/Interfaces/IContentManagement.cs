4352deb65118999ae4f4c2fd1106aec324c4823e
Niklas Hansen <nikl@itu.dk>
2012-05-10 15:04:32 +0200

    Updated documentation for two service endpoints.
    - IUserManagement
    - IContentManagement

--- RentItService/Interfaces/IContentManagement.cs
+++ RentItService/Interfaces/IContentManagement.cs
@@ -17,48 +17,63 @@ namespace RentItService.Interfaces
     public interface IContentManagement
     {
         /// <summary>
-        /// Registers a new movie
+        /// Register a new movie. Must have atleast a title. ReleaseDate can be set 
+        /// to null or a date in the future, meaning the movie has not yet been released.
         /// </summary>
-        /// <param name="token">The user token</param>
+        /// <param name="token">The user's session token.</param>
         /// <param name="movie">The movie to register. Should have a title</param>
-        /// <returns>Wether the request succeeded or not</returns>
+        /// <returns>True on success; false otherwise.</returns>
         [OperationContract]
         bool RegisterMovie(string token, ref Movie movie);
 
         /// <summary>
-        /// Edits a movie
+        /// Edit a movie's information. Title, Description, ImagePath, ReleaseDate 
+        /// and Genres will be updated. Movie will be identified by the ID in the 
+        /// Movie instance passed to the method. The user editing (identified by 
+        /// the token) must be the owner of the movie.
         /// </summary>
-        /// <param name="token">The user token</param>
+        /// <param name="token">The user's session token.</param>
         /// <param name="movie">The movie to edit. Should at least have an ID</param>
-        /// <returns>Wether the request succeeded or not</returns>
+        /// <returns>True on success; false otherwise.</returns>
         [OperationContract]
         bool EditMovie(string token, ref Movie movie);
 
         /// <summary>
-        /// Deletes a movie
+        /// Delete a movie. Movie to delete will be identified by the ID in the 
+        /// Movie instance passed to the method. Nothing more than the ID has to 
+        /// be filled out (will just be ignored).
         /// </summary>
-        /// <param name="token">The user token</param>
-        /// <param name="movie">The movie to delete</param>
-        /// <returns>Wether the request succeeded or not</returns>
+        /// <param name="token">The user's session token.</param>
+        /// <param name="movie">The movie to delete (identified by ID).</param>
+        /// <returns>True on success; false otherwise.</returns>
         [OperationContract]
         bool DeleteMovie(string token, Movie movie);
 
         /// <summary>
-        /// Uploads a movie edition
+        /// Upload a new movie edition. Must set Name and MovieID in the 
+        /// Edition instance passed to the method. The rest will be filled 
+        /// out by the service, and ready to use afterwards. Please be 
+        /// aware that edition name is unique for the movie - meaning that 
+        /// it is not possible to have two different editions for the same 
+        /// movie with the same name. 
+        /// The user creating the edition (identified by the session token) 
+        /// must be the owner of the movie.
         /// </summary>
-        /// <param name="token">The user token</param>
+        /// <param name="token">The user's session token.</param>
         /// <param name="stream">The file stream</param>
         /// <param name="edition">The edition. Should have title and movie</param>
-        /// <returns>Wether the request succeeded or not</returns>
+        /// <returns>True on success; false otherwise.</returns>
         [OperationContract]
         bool UploadEdition(string token, RemoteFileStream stream, ref Edition edition);
 
         /// <summary>
-        /// Deletes a movie edition
+        /// Delete a movie edition. Will remove the movie and all editions of 
+        /// the movie. The user deleting the movie (identified by the session token) 
+        /// must be the owner of the movie.
         /// </summary>
-        /// <param name="token">The user token</param>
+        /// <param name="token">The user's session token.</param>
         /// <param name="edition">The edition to delete</param>
-        /// <returns>Wether the request succeeded or not</returns>
+        /// <returns>True on success; false otherwise.</returns>
         [OperationContract]
         bool DeleteEdition(string token, Edition edition);
     }
--- RentItService/Interfaces/IUserManagement.cs
+++ RentItService/Interfaces/IUserManagement.cs
@@ -6,7 +6,6 @@
 
 namespace RentItService.Interfaces
 {
-    using System.Collections.Generic;
     using System.ServiceModel;
     using Entities;
 
@@ -17,37 +16,43 @@ namespace RentItService.Interfaces
     public interface IUserManagement
     {
         /// <summary>
-        /// Sign up for the service
+        /// Sign up for the service. 
+        /// Put in Username, FullName (optional), Email and Password. The rest 
+        /// of the fields will be reset and updated with the information provided 
+        /// by the service.
         /// </summary>
-        /// <param name="user">The user object. Should have username and password</param>
-        /// <returns>Wether the request succeeded or not</returns>
+        /// <param name="user">The user object. Username, Password and Email are required fields. FullName is optional.</param>
+        /// <returns>True on success; false otherwise.</returns>
         [OperationContract]
         bool SignUp(ref User user);
 
         /// <summary>
-        /// Log in to the system.
+        /// Log in to the system with the user's username and password. 
         /// </summary>
-        /// <param name="username">The username</param>
-        /// <param name="password">The password</param>
-        /// <param name="user">The user's object</param>
-        /// <returns>Wether the request succeeded or not</returns>
+        /// <param name="username">The username of the user.</param>
+        /// <param name="password">The password of the user.</param>
+        /// <param name="user">An instance of the User class, containing all information about that user (including a token).</param>
+        /// <returns>True on success; false otherwise.</returns>
         [OperationContract]
         bool Login(string username, string password, out User user);
 
         /// <summary>
-        /// Log out of the system.
+        /// Log a user out of the system. Will clear the session token.
         /// </summary>
-        /// <param name="token">The user token</param>
-        /// <returns>Wether the request succeeded or not</returns>
+        /// <param name="token">The user's session token.</param>
+        /// <returns>True on success; false otherwise.</returns>
         [OperationContract]
         bool Logout(string token);
 
         /// <summary>
-        /// Edits a user.
+        /// Edit a user's information. The session token must match the user being 
+        /// edited, or the edit will fail. All fields (Email, FullName and Password) 
+        /// will be updated, so remember to fill in all the information - also the 
+        /// old information, if it shouldn't be changed.
         /// </summary>
-        /// <param name="token">The user token</param>
-        /// <param name="user">The user to edit. Should at least have ID.</param>
-        /// <returns>Wether the request succeeded or not</returns>
+        /// <param name="token">The user's session token.</param>
+        /// <param name="user">The user to edit. Should contain all new values (Email, FullName and Password).</param>
+        /// <returns>True on success; false otherwise.</returns>
         [OperationContract]
         bool EditUser(string token, ref User user);
     }
