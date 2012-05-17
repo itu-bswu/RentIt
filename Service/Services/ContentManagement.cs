7f0a4b5db7a83f99f1008f0de5394c9bea17b727
Ulrik Damm <ulrikdamm@me.com>
2012-05-17 10:27:02 +0200

    builds now

--- Service/Services/ContentManagement.cs
+++ Service/Services/ContentManagement.cs
@@ -66,7 +66,7 @@ namespace RentItService.Services
                 return false;
             }
 
-            movie = Movie.Edit(user, movie);
+            movie.Edit(user, movie);
             return true;
         }
 
